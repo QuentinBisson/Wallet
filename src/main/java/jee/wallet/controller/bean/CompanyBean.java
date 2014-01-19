9af868e885728d358c1f8813e5a97f6b01b83268
Grimmar <davidcharpentier@hotmail.fr>
2014-01-19 22:47:59 +0100

    Ajout du graphique dans l'historique

--- src/main/java/jee/wallet/controller/bean/CompanyBean.java
+++ src/main/java/jee/wallet/controller/bean/CompanyBean.java
@@ -2,6 +2,10 @@ package jee.wallet.controller.bean;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -12,20 +16,24 @@ import javax.faces.context.FacesContext;
 import jee.wallet.model.ejb.CompanyEjb;
 import jee.wallet.model.entities.Company;
 import jee.wallet.model.entities.History;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
 
 /**
  *
  * @author Quentin
  */
 public class CompanyBean implements Serializable {
-
+    
     @EJB
     private CompanyEjb companyEjb;
     private Company company;
-
+    private CartesianChartModel historyChartModel;
+    private final static DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
+    
     public void init() {
         ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-        Map<String,String> params = context.getRequestParameterMap();
+        Map<String, String> params = context.getRequestParameterMap();
         company = companyEjb.findById(new Integer(params.get("id")));
         if (!FacesContext.getCurrentInstance()
                 .getPartialViewContext().isAjaxRequest()) {
@@ -39,12 +47,27 @@ public class CompanyBean implements Serializable {
             }
         }
     }
-
+    
     public List<History> getHistory() {
         return company.getHistory();
     }
-
+    
     public Company getCompany() {
         return company;
     }
+    
+    public CartesianChartModel getHistoryChartModel() {
+        historyChartModel = new CartesianChartModel();
+        ChartSeries companyHistorySeries = new ChartSeries();
+        companyHistorySeries.setLabel("Valeur");
+        List<History> history = company.getHistory();
+        for (int i=50; i>0; i--) {
+            History h = history.get(i);
+            companyHistorySeries.set(dateFormat.format(h.getDate()), h.getClose());
+        }
+        
+        historyChartModel.addSeries(companyHistorySeries);
+        return historyChartModel;
+    }
+    
 }
